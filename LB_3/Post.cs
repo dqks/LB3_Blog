03d91f4d8da5a844aa5a94efef0c8252fe32893e
dqks <User@WIN-LFF2FDN918C>
2024-12-10 11:51:15 +0500

    Добавление пакетов. Генерация контекста данных из существующей БД

---
+++ LB_3/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LB_3;
+
+public partial class Post
+{
+    public int Id { get; set; }
+
+    public int IdUser { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Content { get; set; } = null!;
+
+    public DateTime DateOfPublication { get; set; }
+
+    public virtual User IdUserNavigation { get; set; } = null!;
+}
